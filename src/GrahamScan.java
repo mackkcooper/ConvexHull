af7946bbdb48d3efd22a10fd90559b76c260cb7b
mackkcooper <38115302+mackkcooper@users.noreply.github.com>
2019-03-11 18:34:57 -0700

    beginning implementation of GrahamScan

--- src/GrahamScan.java
+++ src/GrahamScan.java
@@ -3,6 +3,38 @@ Author: MacKenzie K. Cooper
 Github: mackkcooper
 */
 
-class GrahamScan {
+import java.util.Vector;
 
+class GrahamScan {
+    private Graph graph = null;
+    private Vector <Vertex> examined = null;
+
+    Graph solve(Graph toSolve) {
+        if(toSolve.vertexCount < 3)
+            return null;
+        graph = new Graph(toSolve);
+        examined = new Vector<>(100);
+        Vertex start = getStart();
+        start.display();
+
+
+        return graph;
+    }
+
+    private Vertex getStart() {
+        Vertex start = null;
+        for(Vertex v : graph.vertices) {
+            if(start == null)
+                start = v;
+            else if(start.y > v.y)
+                start = v;
+            else if(start.y == v.y && start.x > v.x)
+                start = v;
+        }
+        return start;
+    }
+
+    private double polarAngle(Vertex a, Vertex b) {
+        return Math.atan2(b.y-a.y,b.x-a.x);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,7 @@ class Main {
     public static void main(String[] args) {
         Graph graph = new Graph(20,100);
         graph.display();
+        GrahamScan gs = new GrahamScan();
+        System.out.println("Solved: " + gs.solve(graph));
     }
 }
