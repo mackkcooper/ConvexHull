b22422a138249e9848374b5dd8b1e0ba8428e6da
Mack Cooper <maqenzi@gmail.com>
2019-03-13 19:01:42 -0700

    updated main to use new functions

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,12 @@ Github: mackkcooper
 class Main {
 
     public static void main(String[] args) {
-        Graph graph = new Graph(20,100);
-        graph.display();
-        GrahamScan gs = new GrahamScan();
-        System.out.println("Solved: " + gs.solve(graph));
+        Graph graph = new Graph(100,10000);
+        Graph solved = GrahamScan.solve(graph);
+        if(solved != null) {
+            System.out.println("Solved: " + solved.verify());
+            //solved.display();
+            solved.save("plotter/vertices.csv", "plotter/edges.csv");
+        }
     }
 }
